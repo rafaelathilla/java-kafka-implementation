4d7a2726346fbcc35ed97197324ccdf1365d0772
Rafael Athilla Ribeiro de Souza <rafael.souza@ativossa.com.br>
2023-03-21 10:17:02 -0300

    first commit

---
+++ common-kafka/src/main/java/br/com/ecommerce/consumer/ConsumerFunction.java
@@ -0,0 +1,8 @@
+package br.com.ecommerce.consumer;
+
+import br.com.ecommerce.Message;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+
+public interface ConsumerFunction<T> {
+    void consume(ConsumerRecord<String, Message<T>> record) throws Exception;
+}
---
+++ common-kafka/src/main/java/br/com/ecommerce/consumer/ServiceFactory.java
@@ -0,0 +1,5 @@
+package br.com.ecommerce.consumer;
+
+public interface ServiceFactory<T> {
+    ConsumerService<T> create() throws Exception;
+}
